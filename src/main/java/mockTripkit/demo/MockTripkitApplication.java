156c851c97cd8c4333c9b43c9a13b4058e398243
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-12 10:22:05 +0900

    project init

---
+++ src/main/java/mockTripkit/demo/MockTripkitApplication.java
@@ -0,0 +1,13 @@
+package mockTripkit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTripkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTripkitApplication.class, args);
+	}
+
+}
